9f344c7be251af844a1881237f2f9dc1eaeb50ec
KevinGalvis <kevindevelopersr@outlook.com>
2025-02-03 20:44:44 -0500

    agregandoDetalles

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import SobrePage from './Pages/sobreNosotros';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/mundodivertido">
       <NavigationPage />
         <Routes>
           <Route path="/" element={
